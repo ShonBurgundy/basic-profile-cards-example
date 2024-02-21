7bcf0a3801695dfdcfddca3ab2c9c476866593bf
shonburgundy <seanbeloussow@yahoo.com>
2024-02-21 12:01:05 -0800

    feat: add images and Bulma css library

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import "bulma/css/bulma.css";
 import ProfileCard from "./ProfileCard";
 import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
+
+console.log(AlexaImage);
+console.log(SiriImage);
 
 function App() {
   return (
     <>
-      Personal Digital Assitants
-      <ProfileCard title="Alexa" handle="@alexa99" />
-      <ProfileCard title="Cortana" handle="@cortana32" />
-      <ProfileCard title="Siri" handle="@siri01" />
+      <div>Personal Digital Assitants</div>
+
+      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage}/>
+      <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage}/>
+      <ProfileCard title="Siri" handle="@siri01"image={SiriImage} />
     </>
   );
 }
