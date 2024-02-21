d114998ca1954822ac4241bfaa71ee0ba24429fe
shonburgundy <seanbeloussow@yahoo.com>
2024-02-21 14:59:09 -0800

    feat: add description for profile card

--- src/App.js
+++ src/App.js
@@ -10,23 +10,38 @@ console.log(SiriImage);
 function App() {
   return (
     <>
-      <div>Personal Digital Poop</div>
+      <section class="hero is-primary">
+        <div class="hero-body">
+          <p class="title">Personal Digital Assistant</p>
+        </div>
+      </section>
 
       <div className="container">
         <section className="section">
           <div className="columns">
             <div className="column is-4">
-              <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                image={AlexaImage}
+                description="Alexa was createdd by amazona and helps you buy things."
+              />
             </div>
             <div className="column is-4">
               <ProfileCard
                 title="Cortana"
                 handle="@cortana32"
                 image={CortanaImage}
+                description="Cortana was made by microsoft. Who knows what it does?"
               />
             </div>
             <div className="column is-4">
-              <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+              <ProfileCard
+                title="Siri"
+                handle="@siri01"
+                image={SiriImage}
+                description="Siri was made by Apple and is being fazed out."
+              />
             </div>
           </div>
         </section>
